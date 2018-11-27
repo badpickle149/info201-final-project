84fac0a77cc13a54ca8678ebd9f9c60b87dd907e
Thomas Kakatsakis <thomaskakatsakis@gmail.com>
2018-11-26 19:47:19 -0800

    Added specific school function

--- processing.R
+++ processing.R
@@ -4,11 +4,27 @@
 ## query parameters needed for a call to the college scorecard API
 ## returns a list of length 2 where the element "metadata" is a list
 ## describing the data and "results" is a dataframe of the results
-get_dataset <- function(endpoint, params) {
+
+library(httr)
+library(jsonlite)
+
+key = "WxdDlU0dc1U3VPeVJ0Ke2G7QSTPENsSMAfnFKVHV"
+
+get_dataset <- function(params) {
   base <- "https://api.data.gov/ed/collegescorecard"
-  uri <- paste(base, "v1/schools", endpoint, sep = "/")
+  uri <- paste(base, "v1/schools", sep = "/")
   res <- httr::GET(uri, query=params)
   body <- content(res, "text")
   parsed_data <- fromJSON(body)
   return(parsed_data)
 }
+
+# returns the data for a specific school
+data_school <- function(school){
+  params <- list(school.name = school, api_key = key)
+  get_dataset(params)
+}
+
+boston <- data_school("boston college")
+
+
