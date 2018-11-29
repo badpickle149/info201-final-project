2275ddd6989aad6ff53cf388a7c810c7137c534c
Kevin Weng <kevinweng95@gmail.com>
2018-11-29 15:17:47 -0800

    finished layout for comparing two schools

--- server.R
+++ server.R
@@ -1,5 +1,10 @@
 
 
+get_school_name <- function(name) {
+  name
+}
+
 function(input, output, session) {
-  
+  output$school_title_1 <- renderText(get_school_name(input$School1))
+  output$school_title_2 <- renderText(get_school_name(input$School2))
 }
