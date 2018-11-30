ff3a80b41b03e1116b2544dc25c3a701e50cff24
Thomas Kakatsakis <thomaskakatsakis@gmail.com>
2018-11-29 16:06:14 -0800

    added functions

--- processing.R
+++ processing.R
@@ -1,20 +1,21 @@
-## this is the file for getting the data from the API and data wrangling
-
-## this function accepts a list "params" which is a list of the
-## query parameters needed for a call to the college scorecard API
-## returns a list of length 2 where the element "metadata" is a list
-## describing the data and "results" is a dataframe of the results
-
 library(dplyr)
 
 scorecard <- read.csv("data/Most-Recent-Cohorts-Scorecard-Elements.csv", stringsAsFactors = FALSE, header=TRUE, sep=",")
+treasury <- read.csv("data/Most-Recent-Cohorts-Treasury-Elements.csv", stringsAsFactors = FALSE, header=TRUE, sep="," )
 
-# Input school name and vector of criteria to view
-school_info <- function(name, vector) {
-  scorecard %>% filter(INSTNM == name) %>%
+# For scorecard data. Input school name and vector of criteria to view
+school_info_scor <- function(name, vector) {
+  df <- scorecard %>% filter(INSTNM == name) %>%
   select_(.dots=vector)
 }
 
+# For treasury data. Input school name and vector of criteria to view
+school_info_treas <- function(name, vector) {
+  df <- treasury %>% filter(INSTNM == name) %>%
+    select_(.dots=vector)
+}
+
 # Example call of school_info
-harvard_info <- school_info("Harvard University", c("INSTNM", "GRAD_DEBT_MDN_SUPP", "MD_EARN_WNE_P10"))
+harvard_info_score <- school_info_scor("Harvard University", c("INSTNM", "GRAD_DEBT_MDN_SUPP", "MD_EARN_WNE_P10"))
+harvard_info_treas <- school_info_treas("Harvard University", c("INSTNM", "MN_EARN_WNE_P10", "MD_EARN_WNE_P10"))
 
