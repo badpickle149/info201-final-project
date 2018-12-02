a4d23971a9e27cd6a0560f72bbf0eb2956c96982
Kevin Weng <kevinweng95@gmail.com>
2018-12-01 20:42:58 -0800

    fixed small bug in server.R

--- server.R
+++ server.R
@@ -15,12 +15,11 @@ get_school_params <- function(input, type) {
   score_options <- c()
   treasury_options <- c()
   
-  score_values <- c("Median Total Grad Debt", "Median Monthly Payment", "Median Earnings after Graduation", 
-             "% Students Receiving Federal Loan", "% Students Receiving Pell Grant", 
-             "Number of Undergraduates", "School Website Link")
+  score_values <- c("GRAD_DEBT_MDN_SUPP", ##"GRAD_DEBTMDN10YR_SUPP", 
+                      "MD_EARN_WNE_P10", "PCTFLOAN", "PCTPELL", "UGDS", 
+                      "INSTURL")
   
-  treasury_values <- c("Mean Earnings After College", "Unemployment Rate of Students After Graduation", 
-                "Poverty Rate of Students After Graduation")
+  treasury_values <- c("MN_EARN_WNE_P10", "UNEMP_RATE", "POVERTY_RATE")
   
   lapply(input$SchoolOptions, function(x) {
     if (x %in% score_values) {
