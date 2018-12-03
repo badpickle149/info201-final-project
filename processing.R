8f887d4fd91e1f1d857f7ca2bf744c89760eb707
Ryan Tucker <tucker8@uw.edu>
2018-12-03 15:39:49 -0800

    Added Function

--- processing.R
+++ processing.R
@@ -5,41 +5,44 @@
 ## returns a list of length 2 where the element "metadata" is a list
 ## describing the data and "results" is a dataframe of the results
 
-library(httr)
-library(jsonlite)
 library(dplyr)
 
-key = "WxdDlU0dc1U3VPeVJ0Ke2G7QSTPENsSMAfnFKVHV"
+scorecard <- read.csv("./data/Most-Recent-Cohorts-Scorecard-Elements.csv")
+treasury <- read.csv("./data/Most-Recent-Cohorts-Treasury-Elements.csv")
 
-get_dataset <- function(params) {
-  base <- "https://api.data.gov/ed/collegescorecard"
-  uri <- paste(base, "v1/schools", sep = "/")
-  res <- httr::GET(uri, query=params)
-  body <- content(res, "text")
-  parsed_data <- fromJSON(body)
-  return(parsed_data)
+find_school <- function(name)   {
+  scorecard %>% filter(name)
 }
 
-# returns the data for a specific school
-data_school <- function(school){
-  params <- list(school.name = school, api_key = key)
-  data <- get_dataset(params)
-  data <- data$results
+list_best_schools <- function(selected_state)  {
+  treasury_min <- treasury %>% select(INSTNM, MN_EARN_WNE_INC1_P10, 
+                                      MN_EARN_WNE_INC2_P10, MN_EARN_WNE_INC3_P10,
+                                      UNEMP_RATE, MD_EARN_WNE_P10)
+  scorecard_min <- scorecard %>% select(INSTNM, STABBR, GRAD_DEBT_MDN_SUPP)
+  combined_min <- full_join(treasury_min, scorecard_min, by = "INSTNM")
+  combined_min <- combined_min %>%  filter(MN_EARN_WNE_INC1_P10 != "NULL" & 
+                                             MN_EARN_WNE_INC1_P10 != "PrivacySuppressed" &
+                                             MN_EARN_WNE_INC2_P10 != "NULL" &
+                                             MN_EARN_WNE_INC2_P10 != "PrivacySuppressed" &
+                                             MN_EARN_WNE_INC3_P10 != "NULL" &
+                                             MN_EARN_WNE_INC3_P10 != "PrivacySuppressed" &
+                                             UNEMP_RATE != "NULL" &
+                                             UNEMP_RATE != "PrivacySuppressed" &
+                                             MD_EARN_WNE_P10 != "NULL" &
+                                             MD_EARN_WNE_P10 != "PrivacySuppressed" &
+                                             GRAD_DEBT_MDN_SUPP != "NULL" &
+                                             GRAD_DEBT_MDN_SUPP != "PrivacySuppressed")
+  combined_min <- combined_min %>% distinct(INSTNM, MN_EARN_WNE_INC1_P10, MN_EARN_WNE_INC2_P10,
+                                            MN_EARN_WNE_INC3_P10, MD_EARN_WNE_P10, UNEMP_RATE,
+                                            STABBR, GRAD_DEBT_MDN_SUPP)
+  combined_min <- combined_min %>% filter(STABBR == selected_state)
+  combined_min <- combined_min %>% mutate(mean_total = ((as.numeric(as.character(MN_EARN_WNE_INC1_P10)) +
+                                                        as.numeric(as.character(MN_EARN_WNE_INC2_P10)) +
+                                                        as.numeric(as.character(MN_EARN_WNE_INC3_P10))) / 3))
+  combined_min <- combined_min %>% mutate(roi = ((as.numeric(as.character(mean_total)) + 
+                                                 as.numeric(as.character(MD_EARN_WNE_P10))) / 
+                                                 (2 * as.numeric(as.character(GRAD_DEBT_MDN_SUPP)))))
+  best_schools <- combined_min %>% select(INSTNM, mean_total, MD_EARN_WNE_P10, GRAD_DEBT_MDN_SUPP, roi)
+  best_schools <- best_schools %>% arrange(desc(roi)) %>% head(8)
+  return(best_schools)
 }
-
-compare_schools <- function(school1, school2) {
-  school1_data <- data_school(school1)
-  school2_data <- data_school(school2)
-  school1_avg_price <- school1_data$latest$cost$avg_net_price.overall
-  school2_avg_price <- school2_data$latest$cost$avg_net_price.overall
-  
-  combined_data <- inner_join(school1_avg_price, school2_avg_price, by = avg_net_price.overall)
-}
-
-boston <- data_school("boston college")
-boston_admission_details <- head(boston$latest$admissions, 100)
-boston_cost_details <- boston$`2012`$cost
-boston_financial_aid <- boston$latest$aid
-school_sum <- summary(boston_cost_details %>% head(1))
-
-compare <- compare_schools("boston college", "harvard university")
